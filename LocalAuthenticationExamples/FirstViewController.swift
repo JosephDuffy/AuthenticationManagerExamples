289464ef0ae231d77f05bf6dafd215ef124940ba
Joseph Duffy <JosephDuffy@me.com>
2014-07-24 22:02:44 +0100

    Initial commit
    Default tab bar application with iOS 7 deployment target

---
+++ LocalAuthenticationExamples/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LocalAuthenticationExamples
+//
+//  Created by Joseph Duffy on 24/07/2014.
+//  Copyright (c) 2014 Yetii Ltd. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LocalAuthenticationExamples/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LocalAuthenticationExamples
+//
+//  Created by Joseph Duffy on 24/07/2014.
+//  Copyright (c) 2014 Yetii Ltd. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
